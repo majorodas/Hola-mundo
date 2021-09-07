9ffb4517a3e270d9df0bcb21cd5febd91ddf8b24
Majo Rodas <majorodasrosales@gmail.com>
2021-09-06 18:01:22 -0600

    Hola Mundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'holamundo';
-}
+  export class AppComponent {
+    titulo = 'Hola Mundo';
+    count: number = 2100000;
+    countidstudent() : void{
+    //studentname = 'Cesar';
+    //getstudentname (): string {
+      this.count++;
+    }}
+  
+  
+
+
+
